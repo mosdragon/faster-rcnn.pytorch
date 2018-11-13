8bc32a734cd4b3cfe7215dee232baac6ed144a0c
Osama Sakhi <msakhi21@gmail.com>
2018-11-13 15:16:49 -0500

    Fixes for my datasets

--- lib/datasets/pascal_voc_rbg.py
+++ lib/datasets/pascal_voc_rbg.py
@@ -32,14 +32,26 @@ class pascal_voc(imdb):
     self._devkit_path = self._get_default_path() if devkit_path is None \
       else devkit_path
 
-    
+
     self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+    # self._classes = ('__background__',  # always index 0
+    #                  'aeroplane', 'bicycle', 'bird', 'boat',
+    #                  'bottle', 'bus', 'car', 'cat', 'chair',
+    #                  'cow', 'diningtable', 'dog', 'horse',
+    #                  'motorbike', 'person', 'pottedplant',
+    #                  'sheep', 'sofa', 'train', 'tvmonitor')
+
     self._classes = ('__background__',  # always index 0
-                     'aeroplane', 'bicycle', 'bird', 'boat',
-                     'bottle', 'bus', 'car', 'cat', 'chair',
-                     'cow', 'diningtable', 'dog', 'horse',
-                     'motorbike', 'person', 'pottedplant',
-                     'sheep', 'sofa', 'train', 'tvmonitor')
+                            "chair",
+                            "armchair",
+                            "sofa",
+                            "coffee_table",
+                            "desk",
+                            "double_bed",
+                            "single_bed",
+                            "bathtub"
+                    )
+
     self._class_to_ind = dict(list(zip(self.classes, list(range(self.num_classes)))))
     self._image_ext = '.jpg'
     self._image_index = self._load_image_set_index()
@@ -84,7 +96,7 @@ class pascal_voc(imdb):
     # self._devkit_path + /VOCdevkit2007/VOC2007/ImageSets/Main/val.txt
     image_set_file = os.path.join(self._data_path, 'ImageSets', 'Main',
                                   self._image_set + '.txt')
-    
+
     assert os.path.exists(image_set_file), \
       'Path does not exist: {}'.format(image_set_file)
     with open(image_set_file) as f:
@@ -168,10 +180,10 @@ class pascal_voc(imdb):
     for ix, obj in enumerate(objs):
       bbox = obj.find('bndbox')
       # Make pixel indexes 0-based
-      x1 = float(bbox.find('xmin').text) - 1
-      y1 = float(bbox.find('ymin').text) - 1
-      x2 = float(bbox.find('xmax').text) - 1
-      y2 = float(bbox.find('ymax').text) - 1
+      x1 = float(bbox.find('xmin').text)
+      y1 = float(bbox.find('ymin').text)
+      x2 = float(bbox.find('xmax').text)
+      y2 = float(bbox.find('ymax').text)
       cls = self._class_to_ind[obj.find('name').text.lower().strip()]
       boxes[ix, :] = [x1, y1, x2, y2]
       gt_classes[ix] = cls
