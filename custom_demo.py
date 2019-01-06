10c1a1f9932d5bdcb1ebf87c044c38dc943e9262
Osama Sakhi <msakhi21@gmail.com>
2019-01-06 12:30:29 -0500

    Fix

--- custom_demo.py
+++ custom_demo.py
@@ -148,6 +148,11 @@ if __name__ == '__main__':
   print('Called with args:')
   print(args)
 
+  if args.dataset == "pascal_voc":
+    args.imdb_name = "voc_2007_trainval"
+    args.imdbval_name = "voc_2007_test"
+    args.set_cfgs = ['ANCHOR_SCALES', '[16, 32, 64, 128]', 'ANCHOR_RATIOS', '[0.5, 1, 2, 4]']
+
   if args.cfg_file is not None:
     cfg_from_file(args.cfg_file)
   if args.set_cfgs is not None:
