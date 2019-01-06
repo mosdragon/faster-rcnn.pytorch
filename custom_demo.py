3d0a834bf4488ac00d9d8c54c40c4ea66024f744
Osama Sakhi <msakhi21@gmail.com>
2019-01-06 14:52:44 -0500

    Fix

--- custom_demo.py
+++ custom_demo.py
@@ -375,7 +375,7 @@ if __name__ == '__main__':
       # Limit to max_per_image detections *over all classes*
       if max_per_image > 0:
           image_scores = np.hstack([all_boxes[j][i][:, -1]
-                                    for j in xrange(1, imdb.num_classes)])
+                                    for j in xrange(1, num_classes)])
           if len(image_scores) > max_per_image:
               image_thresh = np.sort(image_scores)[-max_per_image]
               for j in xrange(1, num_classes):
